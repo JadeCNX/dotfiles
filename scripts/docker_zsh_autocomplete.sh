82bf546664842cb71755d6d0cbe8adcdfa84b230
jade <jadearmy89@gmail.com>
2019-09-30 11:29:54 +0700

    Change to srcery theme and add docker auto complete script

---
+++ scripts/docker_zsh_autocomplete.sh
@@ -0,0 +1,3 @@
+ln -s /Applications/Docker.app/Contents/Resources/etc/docker.zsh-completion /usr/local/share/zsh/site-functions/_docker
+ln -s /Applications/Docker.app/Contents/Resources/etc/docker-machine.zsh-completion /usr/local/share/zsh/site-functions/_docker-machine
+ln -s /Applications/Docker.app/Contents/Resources/etc/docker-compose.zsh-completion /usr/local/share/zsh/site-functions/_docker-compose
