1ec050ddfa64c539221e2118b5f4b89bd1ab3253
jade <jadearmy89@gmail.com>
2020-01-27 23:09:37 +0700

    Update symlinks

--- symlinks.sh
+++ symlinks.sh
@@ -1,56 +1,77 @@
 #!/bin/sh
 
+backup()
+{
+	file=$1
+	if [ -L $file ] ; then
+		echo "unlink ${file}"
+		unlink -v $file
+	elif [ -e $file ] ; then
+		mv -iv $file{,.old}
+	# else
+	#     echo "Missing"
+	fi
+}
+
 # alacritty
-mv -iv ~/.alacritty.yml{,.old}
-ln -sfnv "$PWD/alacritty.yml" ~/.alacritty.yml
+backup ~/.alacritty.yml
+ln -sfnv $PWD/alacritty.yml ~/.alacritty.yml
 
 # git
-mv -iv ~/.gitignore_global{,.old}
-ln -sfnv "$PWD/gitignore_global" ~/.gitignore_global
+backup ~/.gitignore_global
+ln -sfnv $PWD/gitignore_global ~/.gitignore_global
 
 # hyper
-mv -iv ~/.hyper.js{,.old}
-ln -sfnv "$PWD/hyper.js" ~/.hyper.js
+backup ~/.hyper.js
+ln -sfnv $PWD/hyper.js ~/.hyper.js
 
 # emacs
-mv -iv ~/.spacemacs.d ~/.{,.old}
-ln -sfnv "$PWD/spacemacs.d" ~/.spacemacs.d
+backup ~/.spacemacs.d
+ln -sfnv $PWD/spacemacs.d ~/.spacemacs.d
 
 # tmux
-ln -sfnv "$PWD/tmux.conf" ~/.tmux.conf
+backup ~/.tmux.conf
+ln -sfnv $PWD/tmux.conf ~/.tmux.conf
 
 # eclipse - vrapper
-mv -iv ~/.vrapperrc{,.old}
-ln -sfnv "$PWD/vrapperrc" ~/.vrapperrc
+backup ~/.vrapperrc
+ln -sfnv $PWD/vrapperrc ~/.vrapperrc
 
 # w3m
-mv -iv ~/.w3m{,.old}
-ln -sfnv "$PWD/w3m/w3m_config" ~/.w3m/config
-ln -sfnv "$PWD/w3m/w3m_keymap" ~/.w3m/keymap
+backup ~/.w3m
+mkdir -pv ~/.w3m
+ln -sfnv $PWD/w3m/w3m_config ~/.w3m/config
+ln -sfnv $PWD/w3m/w3m_keymap ~/.w3m/keymap
 
 # xcode - xvim2
-mv -iv ~/.xvimrc{,.old}
-ln -sfnv "$PWD/xvimrc" ~/.xvimrc
+backup ~/.xvimrc
+ln -sfnv $PWD/xvimrc ~/.xvimrc
 
 # vim
-mv -iv ~/.tmux.conf{,.old}
-mv -iv ~/.config/nvim{,.old}
+backup ~/.vimrc
+backup ~/.config/nvim
 
-mkdir -p "$PWD/.config"
-ln -sfnv "$PWD/vimrcs/config_nvim" ~/.config/nvim
-ln -sfnv "$PWD/vimrcs/vimrc.vim" ~/.vimrc
+mkdir -pv ~/.config
+ln -sfnv $PWD/vimrcs/config_nvim ~/.config/nvim
+ln -sfnv $PWD/vimrcs/vimrc.vim ~/.vimrc
 
 # zprezto
-mv -iv ~/.zlogin{,.old}
-mv -iv ~/.zlogout{,.old}
-mv -iv ~/.zpreztorc{,.old}
-mv -iv ~/.zprofile{,.old}
-mv -iv ~/.zshenv{,.old}
-mv -iv ~/.zshrc{,.old}
+backup ~/.zlogin
+backup ~/.zlogout
+backup ~/.zpreztorc
+backup ~/.zprofile
+backup ~/.zshenv
+backup ~/.zshrc
 
-ln -sfnv "$PWD/zprezto/zlogin" ~/.zlogin
-ln -sfnv "$PWD/zprezto/zlogout" ~/.zlogout
-ln -sfnv "$PWD/zprezto/zpreztorc" ~/.zpreztorc
-ln -sfnv "$PWD/zprezto/zprofile" ~/.zprofile
-ln -sfnv "$PWD/zprezto/zshenv" ~/.zshenv
-ln -sfnv "$PWD/zprezto/zshrc" ~/.zshrc
+ln -sfnv $PWD/zprezto/zlogin ~/.zlogin
+ln -sfnv $PWD/zprezto/zlogout ~/.zlogout
+ln -sfnv $PWD/zprezto/zpreztorc ~/.zpreztorc
+ln -sfnv $PWD/zprezto/zprofile ~/.zprofile
+ln -sfnv $PWD/zprezto/zshenv ~/.zshenv
+ln -sfnv $PWD/zprezto/zshrc ~/.zshrc
+
+# hammperspoon
+backup ~/.hammerspoon
+ln -sfnv $PWD/hammerspoon ~/.hammerspoon
+
+
