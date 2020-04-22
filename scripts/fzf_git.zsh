6aaed312a6b204b7c64a1a89b31b435c80baa43d
jade <jadearmy89@gmail.com>
2020-04-22 11:01:58 +0700

    Update

--- scripts/fzf_git.zsh
+++ scripts/fzf_git.zsh
@@ -145,10 +145,11 @@ __fgst() {
     # "Nothing to see here, move along"
     f-is_in_git_repo || return
 
-    local cmd="${FZF_CTRL_T_COMMAND:-"command git status -s"}"
+    local cmd="${FZF_CTRL_T_COMMAND:-"command git status -s | sed s/^...//"}"
 
     eval "$cmd" | FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-40%} --reverse $FZF_DEFAULT_OPTS $FZF_CTRL_T_OPTS" fzf -m "$@" | while read -r item; do
-		printf '%q ' "$item" | cut -d " " -f 2
+		# printf '%q ' "$item" | cut -d " " -f 2
+        echo -n "${(q)item} "
 	done
 	local ret=$?
 	echo
