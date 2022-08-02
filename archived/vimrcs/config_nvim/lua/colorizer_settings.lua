84d7d114dcb18a7b528045ac02a2aabbc85cfb44
jade <jadearmy89@gmail.com>
2022-08-02 14:06:18 +0700

    chore: remove unused

--- archived/vimrcs/config_nvim/lua/colorizer_settings.lua
+++
@@ -1,8 +0,0 @@
-local colorizer = require 'colorizer'
-colorizer.setup({
-	'*';
-	css = { css = true };
-	scss = { css = true };
-	less = { css = true };
-	thml = { css = true };
-}, { mode = 'background' })
