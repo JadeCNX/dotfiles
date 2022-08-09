a1ea3e61401f1fcf01a0e2a8ebbb49fa5347e730
jade <jadearmy89@gmail.com>
2022-08-09 18:11:57 +0700

    fix: unable haskell syntax

--- lunarvim/config.lua
+++ lunarvim/config.lua
@@ -244,7 +244,7 @@ lvim.builtin.gitsigns.current_line_blame_opts = {
 
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = "all"
-lvim.builtin.treesitter.ignore_install = { "haskell" }
+-- lvim.builtin.treesitter.ignore_install = { "haskell" }
 lvim.builtin.treesitter.highlight.enabled = true
 lvim.builtin.treesitter.rainbow.enable = true
 
