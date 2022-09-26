276f39ea6600eebd79621f4b034e936437ba6719
jade <jadearmy89@gmail.com>
2022-09-26 11:16:34 +0700

    feat: refactoring keymap

--- lunarvim/config.lua
+++ lunarvim/config.lua
@@ -732,7 +732,7 @@ lvim.plugins = {
       )
       vim.api.nvim_set_keymap(
         "v",
-        "<leader>lf",
+        "<leader>lE",
         [[ <Esc><Cmd>lua require('refactoring').refactor('Extract Function To File')<CR>]],
         { noremap = true, silent = true, expr = false }
       )
