87411598ac200e075ebd94adeb0e7910910a8a5d
jade <jadearmy89@gmail.com>
2024-01-06 16:37:46 +0700

    fix: auto dark mode script not work on crontab

--- scripts/toggle-dark-mode.sh
+++ scripts/toggle-dark-mode.sh
@@ -6,11 +6,11 @@ DARK_MODE=$(defaults read -g AppleInterfaceStyle 2>&1)
 # echo "DARK_MODE=$DARK_MODE"
 
 if [ "$DARK_MODE" = "Dark" ]; then
-  gsed -i -r 's/catppuccin-\w+/catppuccin-mocha/' ~/.config/alacritty/alacritty.toml
-  gsed -i -r "s/catppuccin_flavour '\\w+'/catppuccin_flavour 'mocha'/" ~/.tmux.conf
+  /opt/homebrew/bin/gsed -i -r 's/catppuccin-\w+/catppuccin-mocha/' ~/.config/alacritty/alacritty.toml
+  /opt/homebrew/bin/gsed -i -r "s/catppuccin_flavour '\\w+'/catppuccin_flavour 'mocha'/" ~/.tmux.conf
 else
-  gsed -i -r 's/catppuccin-\w+/catppuccin-latte/' ~/.config/alacritty/alacritty.toml
-  gsed -i -r "s/catppuccin_flavour '\\w+'/catppuccin_flavour 'latte'/" ~/.tmux.conf
+  /opt/homebrew/bin/gsed -i -r 's/catppuccin-\w+/catppuccin-latte/' ~/.config/alacritty/alacritty.toml
+  /opt/homebrew/bin/gsed -i -r "s/catppuccin_flavour '\\w+'/catppuccin_flavour 'latte'/" ~/.tmux.conf
 fi
 
-tmux source-file ~/.tmux.conf
+/opt/homebrew/bin/tmux source-file ~/.tmux.conf
