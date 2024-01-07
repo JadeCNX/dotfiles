4bd405bef45ff43b8b1dd738cd58f5be1593619e
jade <jadearmy89@gmail.com>
2024-01-07 11:06:03 +0700

    feat: add tmux auto dark mode

--- scripts/toggle-dark-mode.sh
+++ scripts/toggle-dark-mode.sh
@@ -2,15 +2,13 @@
 
 osascript -e 'tell app "System Events" to tell appearance preferences to set dark mode to not dark mode'
 
-DARK_MODE=$(defaults read -g AppleInterfaceStyle 2>&1)
+# DARK_MODE=$(defaults read -g AppleInterfaceStyle 2>&1)
 # echo "DARK_MODE=$DARK_MODE"
 
-if [ "$DARK_MODE" = "Dark" ]; then
-  /opt/homebrew/bin/gsed -i -r 's/catppuccin-\w+/catppuccin-mocha/' ~/.config/alacritty/alacritty.toml
-  /opt/homebrew/bin/gsed -i -r "s/catppuccin_flavour '\\w+'/catppuccin_flavour 'mocha'/" ~/.tmux.conf
-else
-  /opt/homebrew/bin/gsed -i -r 's/catppuccin-\w+/catppuccin-latte/' ~/.config/alacritty/alacritty.toml
-  /opt/homebrew/bin/gsed -i -r "s/catppuccin_flavour '\\w+'/catppuccin_flavour 'latte'/" ~/.tmux.conf
-fi
+# if [ "$DARK_MODE" = "Dark" ]; then
+#   /opt/homebrew/bin/gsed -i -r 's/catppuccin-\w+/catppuccin-mocha/' ~/.config/alacritty/alacritty.toml
+# else
+#   /opt/homebrew/bin/gsed -i -r 's/catppuccin-\w+/catppuccin-latte/' ~/.config/alacritty/alacritty.toml
+# fi
 
-/opt/homebrew/bin/tmux source-file ~/.tmux.conf
+# /opt/homebrew/bin/tmux source-file ~/.tmux.conf
