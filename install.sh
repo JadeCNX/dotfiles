d616185d635ce2db0d225b55c85deec5bc9464b5
jade <jadearmy89@gmail.com>
2024-01-08 11:24:32 +0700

    feat: update dark mode

--- install.sh
+++ install.sh
@@ -25,71 +25,80 @@ link() {
   ln -sfn "$source" "$destination"
 }
 
+copy() {
+  config=$1
+  source=$2
+  destination=$3
+  if [[ -n "$specific_config" ]] && [[ "$specific_config" != "$config" ]]; then
+    return
+  fi
+  echo "SET: $config - $destination"
+  rsync -azv "$source" "$destination"
+}
+
 # alacritty
-link alacritty "$PWD"/alacritty/alacritty.toml ~/.config/alacritty/alacritty.toml
-link alacritty "$PWD"/alacritty/catppuccin-frappe.toml ~/.config/alacritty/catppuccin-frappe.toml
-link alacritty "$PWD"/alacritty/catppuccin-latte.toml ~/.config/alacritty/catppuccin-latte.toml
-link alacritty "$PWD"/alacritty/catppuccin-macchiato.toml ~/.config/alacritty/catppuccin-macchiato.toml
-link alacritty "$PWD"/alacritty/catppuccin-mocha.toml ~/.config/alacritty/catppuccin-mocha.toml
+copy alacritty "$PWD/alacritty" "$HOME/.config/"
 
 # git
-link git "$PWD"/gitignore_global ~/.gitignore_global
+link git "$PWD/gitignore_global" "$HOME/.gitignore_global"
 
 # hyper
-link hyper "$PWD"/hyper.js ~/.hyper.js
+link hyper "$PWD/hyper.js" "$HOME/.hyper.js"
 
 # emacs
-link emacs "$PWD"/spacemacs.d/init.el ~/.spacemacs.d/init.el
+link emacs "$PWD/spacemacs.d/init.el" "$HOME/.spacemacs.d/init.el"
 
 # tmux
-link tmux "$PWD"/tmux/tmux.conf ~/.tmux.conf
+link tmux "$PWD/tmux/tmux.conf" "$HOME/.tmux.conf"
+link tmux "$PWD/tmux/themes/light.conf" "$HOME/.tmux/themes/light.conf"
+link tmux "$PWD/tmux/themes/dark.conf" "$HOME/.tmux/themes/dark.conf"
 
 # vim
-link vim "$PWD"/vimrcs/vimrc.vim ~/.vimrc
+link vim "$PWD/vimrcs/vimrc.vim" "$HOME/.vimrc"
 
 # htop
-link htop "$PWD"/htoprc ~/.config/htop/htoprc
+link htop "$PWD/htoprc" "$HOME/.config/htop/htoprc"
 
 # zprezto
-link zprezto "$PWD"/zprezto/zlogin ~/.zlogin
-link zprezto "$PWD"/zprezto/zlogout ~/.zlogout
-link zprezto "$PWD"/zprezto/zpreztorc ~/.zpreztorc
-link zprezto "$PWD"/zprezto/zprofile ~/.zprofile
-link zprezto "$PWD"/zprezto/zshenv ~/.zshenv
-link zprezto "$PWD"/zprezto/zshrc ~/.zshrc
-link zprezto "$PWD"/scripts/fzf_git.zsh ~/.fzf_git.zsh
+link zprezto "$PWD/zprezto/zlogin" "$HOME/.zlogin"
+link zprezto "$PWD/zprezto/zlogout" "$HOME/.zlogout"
+link zprezto "$PWD/zprezto/zpreztorc" "$HOME/.zpreztorc"
+link zprezto "$PWD/zprezto/zprofile" "$HOME/.zprofile"
+link zprezto "$PWD/zprezto/zshenv" "$HOME/.zshenv"
+link zprezto "$PWD/zprezto/zshrc" "$HOME/.zshrc"
+link zprezto "$PWD/scripts/fzf_git.zsh" "$HOME/.fzf_git.zsh"
 
 # bat
-link bat "$PWD"/bat/config ~/.config/bat/config
+link bat "$PWD/bat/config" "$HOME/.config/bat/config"
 
 # starship https://starship.rs
-link starship  "$PWD"/starship.toml ~/.config/starship.toml
+link starship  "$PWD/starship.toml" "$HOME/.config/starship.toml"
 
 # lf
-link lf "$PWD"/lf/lfrc ~/.config/lf/lfrc
-link lf "$PWD"/lf/preview ~/.config/lf/preview
-link lf "$PWD"/lf/cleaner ~/.config/lf/cleaner
-link lf "$PWD"/lf/lfrun ~/.local/bin/lfrun
+link lf "$PWD/lf/lfrc" "$HOME/.config/lf/lfrc"
+link lf "$PWD/lf/preview" "$HOME/.config/lf/preview"
+link lf "$PWD/lf/cleaner" "$HOME/.config/lf/cleaner"
+link lf "$PWD/lf/lfrun" "$HOME/.local/bin/lfrun"
 
 # kitty
-link kitty "$PWD"/kitty.conf ~/.config/kitty/kitty.conf
+link kitty "$PWD/kitty.conf" "$HOME/.config/kitty/kitty.conf"
 
 # mpv
-link mpv "$PWD"/mpv/mpv.conf ~/.config/mpv/mpv.conf
-link mpv "$PWD"/mpv/input.conf ~/.config/mpv/input.conf
+link mpv "$PWD/mpv/mpv.conf" "$HOME/.config/mpv/mpv.conf"
+link mpv "$PWD/mpv/input.conf" "$HOME/.config/mpv/input.conf"
 
 # fish
-link fish "$PWD"/fish/config.fish ~/.config/fish/config.fish
-link fish "$PWD"/fish/fish_plugins ~/.config/fish/fish_plugins
+link fish "$PWD/fish/config.fish" "$HOME/.config/fish/config.fish"
+link fish "$PWD/fish/fish_plugins" "$HOME/.config/fish/fish_plugins"
 
 # ideavimrc
-link ideavim "$PWD"/ideavimrc ~/.ideavimrc
+link ideavim "$PWD/ideavimrc" "$HOME/.ideavimrc"
 
 # vrapper
-link vrapper "$PWD"/vrapperrc ~/.vrapperrc
+link vrapper "$PWD/vrapperrc" "$HOME/.vrapperrc"
 
 # zellij
-link zellij "$PWD"/zellij/config.kdl ~/.config/zellij/config.kdl
+link zellij "$PWD/zellij/config.kdl" "$HOME/.config/zellij/config.kdl"
 
 # wezterm
-link wezterm "$PWD"/wezterm.lua ~/.wezterm.lua
+link wezterm "$PWD/wezterm.lua" "$HOME/.wezterm.lua"
