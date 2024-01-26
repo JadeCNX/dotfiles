47e89a2d4d3da57d29ddf9aafe7d38ee86415f58
jade <jadearmy89@gmail.com>
2024-01-26 11:59:54 +0700

    feat: update tmux configs

--- tmux/themes/toggle-themes.sh
+++ tmux/themes/toggle-themes.sh
@@ -6,12 +6,12 @@ if [ "$DARK_MODE" = "dark" ]; then
   tmux set -g @catppuccin_flavour 'mocha'
   tmux set -g @rose_pine_variant 'moon'
 
-  /opt/homebrew/bin/gsed -i -r 's/themes-\w+/dark-theme/' "$HOME/.config/alacritty/toggle-theme.toml"
+  /opt/homebrew/bin/gsed -i -r 's/\w+-theme/dark-theme/' "$HOME/.config/alacritty/themes/toggle-theme.toml"
 else 
   tmux set -g @catppuccin_flavour 'latte'
   tmux set -g @rose_pine_variant 'dawn'
 
-  /opt/homebrew/bin/gsed -i -r 's/themes-\w+/light-theme/' "$HOME/.config/alacritty/toggle-theme.toml"
+  /opt/homebrew/bin/gsed -i -r 's/\w+-theme/light-theme/' "$HOME/.config/alacritty/themes/toggle-theme.toml"
 fi
 
 /opt/homebrew/bin/tmux run-shell "$HOME/.tmux/plugins/tmux/catppuccin.tmux"
