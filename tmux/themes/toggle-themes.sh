6620da80653722878604ad5afa36f65cc127cab9
jade <jadearmy89@gmail.com>
2024-08-19 02:20:46 +0700

    feat: Use symbolic links for alacritty themes
    This commit changes the way we handle alacritty themes. Instead of
    copying the theme files, we now create symbolic links. This makes it
    easier to update the themes as we can just update the source file and
    the changes will be reflected in the linked file. Also, the redundant
    variable `alacritty_config_file` is removed and its usage is replaced
    with a direct path.

--- tmux/themes/toggle-themes.sh
+++ tmux/themes/toggle-themes.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 
 dark_mode="$1"
+
 alacritty_theme_file="$HOME/.config/alacritty/theme.toml"
-alacritty_config_file="$HOME/.config/alacritty/alacritty.toml"
 
 if [ "$dark_mode" = "dark" ]; then
   tmux set -g @catppuccin_flavour 'mocha'
   tmux set -g @rose_pine_variant 'moon'
 
-  cp "$HOME/.config/alacritty/themes/monokai_pro.toml" "$alacritty_theme_file"
+  ln -fs "$HOME/.config/alacritty/themes/monokai_pro.toml" "$alacritty_theme_file"
   /opt/homebrew/bin/tmux run-shell "$HOME/.tmux/plugins/tmux-monokai-pro/monokai.tmux"
 else
   tmux set -g @catppuccin_flavour 'latte'
   tmux set -g @rose_pine_variant 'dawn'
 
-  cp "$HOME/.config/alacritty/themes/rose-pine-dawn.toml" "$alacritty_theme_file"
+  ln -fs "$HOME/.config/alacritty/themes/rose-pine-dawn.toml" "$alacritty_theme_file"
   /opt/homebrew/bin/tmux run-shell "$HOME/.tmux/plugins/tmux/rose-pine.tmux"
 fi
 
-touch "$alacritty_config_file"
+touch "$HOME/.config/alacritty/alacritty.toml"
