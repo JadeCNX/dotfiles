b4d466defe3d72c57d6c7fe7789d608c6045f15f
jade <jadearmy89@gmail.com>
2024-11-29 17:31:07 +0700

    fix(tmux): correct flavor spelling and add passthrough
    Corrected the spelling of 'flavour' to 'flavor' in toggle-themes.sh.
    Added 'allow-passthrough' and 'visual-activity' settings in tmux.conf.

--- tmux/themes/toggle-themes.sh
+++ tmux/themes/toggle-themes.sh
@@ -5,13 +5,13 @@ dark_mode="$1"
 alacritty_theme_file="$HOME/.config/alacritty/theme.toml"
 
 if [ "$dark_mode" = "dark" ]; then
-  tmux set -g @catppuccin_flavour 'mocha'
+  tmux set -g @catppuccin_flavor 'mocha'
   tmux set -g @rose_pine_variant 'moon'
 
   ln -fs "$HOME/.config/alacritty/themes/catppuccin_mocha.toml" "$alacritty_theme_file"
   /opt/homebrew/bin/tmux run-shell "$HOME/.tmux/plugins/tmux/catppuccin.tmux"
 else
-  tmux set -g @catppuccin_flavour 'latte'
+  tmux set -g @catppuccin_flavor 'latte'
   tmux set -g @rose_pine_variant 'dawn'
 
   ln -fs "$HOME/.config/alacritty/themes/catppuccin_latte.toml" "$alacritty_theme_file"
