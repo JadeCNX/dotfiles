2308d44bbad7e2951dd1a9d30a95b749e4ec537d
jade <jadearmy89@gmail.com>
2025-06-28 18:54:15 +0700

    feat: improve config and formatting consistency
    - Removed unused terminal color settings for cleaner config.
    - Simplified file existence check logic for ruby path.
    - Ensured consistent column width in stylua.toml for formatting.

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -82,42 +82,6 @@ if vim.g.neovide then
   vim.api.nvim_set_keymap("t", "<D-v>", "<C-R>*", { noremap = true, silent = true })
   vim.api.nvim_set_keymap("v", "<D-v>", "<C-R>*", { noremap = true, silent = true })
 
-  -- vim.g.terminal_color_0 = "#2e3436"
-  -- vim.g.terminal_color_1 = "#cc0000"
-  -- vim.g.terminal_color_2 = "#4e9a06"
-  -- vim.g.terminal_color_3 = "#c4a000"
-  -- vim.g.terminal_color_4 = "#3465a4"
-  -- vim.g.terminal_color_5 = "#75507b"
-  -- vim.g.terminal_color_6 = "#0b939b"
-  -- vim.g.terminal_color_7 = "#d3d7cf"
-  -- vim.g.terminal_color_8 = "#555753"
-  -- vim.g.terminal_color_9 = "#ef2929"
-  -- vim.g.terminal_color_10 = "#8ae234"
-  -- vim.g.terminal_color_11 = "#fce94f"
-  -- vim.g.terminal_color_12 = "#729fcf"
-  -- vim.g.terminal_color_13 = "#ad7fa8"
-  -- vim.g.terminal_color_14 = "#00f5e9"
-  -- vim.g.terminal_color_15 = "#eeeeec"
-end
-
-local function file_exists(name)
-  local f = io.open(name, "r")
-  if f ~= nil then
-    io.close(f)
-    return true
-  else
-    return false
-  end
-end
-
--- local python_path = "/opt/homebrew/bin/python3"
--- if file_exists(python_path) then
---   vim.g.python3_host_prog = python_path
--- end
-
-local ruby_path = "/opt/homebrew/opt/ruby/bin/ruby"
-if file_exists(ruby_path) then
-  vim.g.ruby_host_prog = ruby_path
 end
 
 -- scroll off
