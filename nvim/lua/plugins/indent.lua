48ae3baf8504f27d5edd2b78be0a292e2503e997
jade <jadearmy89@gmail.com>
2025-08-14 14:19:39 +0700

    Configure guess-indent.nvim with custom options
    - Switch from init to config for plugin setup.
    - Exclude specific filetypes and buffer types from indentation guessing.
    - Set tab and space options for detected indentation.
    - Disable override of editorconfig settings.

--- nvim/lua/plugins/indent.lua
+++ nvim/lua/plugins/indent.lua
@@ -1,8 +1,34 @@
 return {
   {
     "nmac427/guess-indent.nvim",
-    init = function()
-      require("guess-indent").setup({})
+    config = function()
+      require("guess-indent").setup({
+        auto_cmd = true,
+        override_editorconfig = false,
+        filetype_exclude = {
+          "netrw",
+          "tutor",
+          "grug-far",
+          "snacks_picker_input",
+          "codecompanion",
+          "copilot-chat",
+        },
+        buftype_exclude = {
+          "help",
+          "nofile",
+          "terminal",
+          "prompt",
+        },
+        on_tab_options = {
+          ["expandtab"] = false,
+        },
+        on_space_options = {
+          ["expandtab"] = true,
+          ["tabstop"] = "detected",
+          ["softtabstop"] = "detected",
+          ["shiftwidth"] = "detected",
+        },
+      })
     end,
   },
 }
