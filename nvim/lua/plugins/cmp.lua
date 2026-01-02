04c15169905cf692f293be0a5bb7e8eb938fb56d
jade <jadearmy89@gmail.com>
2026-01-02 23:16:54 +0700

    Simplify blink.cmp plugin configuration
    - Remove commented-out and unused configuration code.
    - Provide a minimal configuration with only the keymap preset.

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -1,46 +1,10 @@
-return {}
--- return {
---   {
---     "saghen/blink.cmp",
---     lazy = true,
---     opts = function(_, o)
---       local conf = {
---         completion = {
---           -- documentation = { auto_show = false },
---           trigger = {
---             show_in_snippet = false,
---           },
---           snippets = {
---             expand = function(snippet, _)
---               return LazyVim.cmp.expand(snippet)
---             end,
---           },
---           list = {
---             selection = {
---               preselect = function()
---                 return not require("blink.cmp").snippet_active({ direction = 1 })
---               end,
---             },
---           },
---         },
---         keymap = {
---           preset = "super-tab",
---           ["<Tab>"] = {
---             function(cmp)
---               if cmp.snippet_active() then
---                 return cmp.accept()
---               else
---                 return cmp.select_and_accept()
---               end
---             end,
---             require("lazyvim.util.cmp").map({ "snippet_forward", "ai_accept" }),
---             "fallback",
---           },
---           ["<C-i>"] = { "show", "show_documentation", "hide_documentation" },
---         },
---       }
---
---       return vim.tbl_deep_extend("force", o, conf)
---     end,
---   },
--- }
+return {
+  {
+    "saghen/blink.cmp",
+    opts = {
+      keymap = {
+        preset = "super-tab",
+      },
+    },
+  },
+}
