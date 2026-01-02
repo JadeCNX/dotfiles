039ef440ffb2bed3d37aa3fcf6de5a8b50151cf8
jade <jadearmy89@gmail.com>
2026-01-02 23:21:35 +0700

    Enable signature help and customize appearance in cmp config
    - Turn on experimental signature help support in cmp.
    - Set nerd font variant to "normal" for improved appearance.

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -2,9 +2,14 @@ return {
   {
     "saghen/blink.cmp",
     opts = {
+      appearance = {
+        nerd_font_variant = "normal",
+      },
       keymap = {
         preset = "super-tab",
       },
+      -- experimental signature help support
+      signature = { enabled = true },
     },
   },
 }
